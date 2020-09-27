5bc2dde64e41c51127c6656da1253a5493e0adb9
kyj924 <yongjoo0924@gmail.com>
2020-09-27 23:45:58 +0900

    last

--- index.js
+++ index.js
@@ -68,22 +68,22 @@ client.on('message', (message) => {
         return;
       }
       if (rper == 0) {
-        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[1]+" "+text2[3]+" "+text2[5]+" "+text2[7]+" "+text2[9]+" \n 2팀: "+text2[2]+" "+text2[4]+" "+text2[6]+" "+text2[8]+" "+text2[10]+"입니다.")
+        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[1]+ +text2[3]+ +text2[5]+ +text2[7]+ +text2[9]+" \n 2팀: "+text2[2]+ +text2[4]+ +text2[6]+ +text2[8]+ +text2[10]+"입니다.")
       }
   
       if(rper == 1) {
-        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[2]+" "+text2[4]+" "+text2[6]+" "+text2[8]+" "+text2[10]+" \n 2팀: "+text2[1]+" "+text2[3]+" "+text2[5]+" "+text2[7]+" "+text2[9]+"입니다.")
+        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[2]+ +text2[4]+ +text2[6]+ +text2[8]+ +text2[10]+" \n 2팀: "+text2[1]+ +text2[3]+ +text2[5]+ +text2[7]+ +text2[9]+"입니다.")
       }
 
       if(rper == 2) {
-        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[1]+" "+text2[4]+" "+text2[5]+" "+text2[8]+" "+text2[9]+" \n 2팀: "+text2[2]+" "+text2[3]+" "+text2[6]+" "+text2[7]+" "+text2[10]+"입니다.")
+        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[1]+ +text2[4]+ +text2[5]+ +text2[8]+ +text2[9]+" \n 2팀: "+text2[2]+ +text2[3]+ +text2[6]+ +text2[7]+ +text2[10]+"입니다.")
       }
       
       if(rper == 3) {
-        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[2]+" "+text2[3]+" "+text2[6]+" "+text2[7]+" "+text2[10]+" \n 2팀: "+text2[1]+" "+text2[4]+" "+text2[5]+" "+text2[8]+" "+text2[9]+"입니다.")
+        message.channel.send(" 사다리 결과는 \n 1팀: "+text2[2]+ +text2[3]+ +text2[6]+ +text2[7]+ +text2[10]+" \n 2팀: "+text2[1]+ +text2[4]+ +text2[5]+ +text2[8]+ +text2[9]+"입니다.")
       }
 
-      if (isundefined(text2)) {
+      if (isNaN(text2)) {
        text2 = 0;
       }
     }
